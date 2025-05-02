779b9a16772d1dc25397de947d967fd7168e0578
ripmyloven <ripmyloven@gmail.com>
2025-05-02 17:54:41 +0300

    rethinkdb for animessanger project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        string rethinkPath = Path.Combine(AppContext.BaseDirectory, "Resources", "Executables", "rethinkdb.exe");
+
+        if (!File.Exists(rethinkPath))
+        {
+            Console.WriteLine($"[!] rethinkdb.exe не найден: {rethinkPath}");
+            return;
+        }
+
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = rethinkPath,
+                Arguments = "--directory rethinkdb_data", // или другой каталог, если хочешь
+                WorkingDirectory = Path.GetDirectoryName(rethinkPath)!,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+
+        process.Start();
+        Console.WriteLine("[✓] rethinkdb.exe запущен");
+
+        await Task.Delay(-1); // оставим процесс живым
+    }
+}
